4dd4cc243d9f69961989b3ba1635d074e93d7f3e
micnori <micnori@users.noreply.github.com>
2024-12-18 11:22:51 +0100

    Update conf.py

--- util/conf.py
+++ util/conf.py
@@ -461,7 +461,7 @@ def get_args(initial_args=None):
     conf["dec_voc_size"] = 32
 
     if args.bpe == True:
-        conf["sp"] = spm.SentencePieceProcessor('./sentencepiece/build/libri.bpe-256.model')
+        conf["sp"] = spm.SentencePieceProcessor('/shared/sentencepiece/build/libri.bpe-256.model')
         #conf["sp"].load('./sentencepiece/build/libri.bpe-256.model')
         conf["src_pad_idx"] = 0
         conf["trg_pad_idx"] = 126
@@ -469,8 +469,8 @@ def get_args(initial_args=None):
         conf["trg_eos_idx"] = 2
         conf["enc_voc_size"] = conf["sp"].get_piece_size()
         conf["dec_voc_size"] = conf["sp"].get_piece_size()
-        conf["lexicon"] = "./sentencepiece/build/librispeech-bpe-256.lex"
-        conf["tokens"] = "./sentencepiece/build/librispeech-bpe-256.tok"
+        conf["lexicon"] = "/shared/sentencepiece/build/librispeech-bpe-256.lex"
+        conf["tokens"] = "/shared/sentencepiece/build/librispeech-bpe-256.tok"
  
     conf["inf"] = float('inf')
 
